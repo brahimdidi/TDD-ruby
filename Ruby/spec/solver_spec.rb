5b83599b6e4fa8b5db24fc39e4b2a4196cd642ff
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-11 20:06:29 +0430

    add test for reverse method that should return a reverse of given string

--- Ruby/spec/solver_spec.rb
+++ Ruby/spec/solver_spec.rb
@@ -6,7 +6,7 @@ describe Solver do
     solver = Solver.new()
   end
 
-  context  "factorial(n) should return the factorial of n" do
+  context "factorial(n) should return the factorial of n" do
 
     it "thorws error when n is less than 0" do
       expect{solver.factorial(-1)}.to raise_exception ArgumentError
@@ -19,6 +19,14 @@ describe Solver do
     it "return the right factorial value for n=3" do
       result = solver.factorial(3);
       expected_result = 6
+      expect(result).to eql(expected_result)
+    end
+  end
+
+  context "return a reverse for a given string" do
+    it "return the reverse of 'hello' string" do
+      result = solver.reverse("hello")
+      expected_result = "olleh"
       expect(result).to eql(expected_result)
     end
   end
