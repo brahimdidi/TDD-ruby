a2d68be85fb4fee13ee4027875feca4bffca991d
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-11 15:46:06 +0000

    add test for method fizzbuzz for n divisble by 3

--- Ruby/spec/solver_spec.rb
+++ Ruby/spec/solver_spec.rb
@@ -6,7 +6,7 @@ describe Solver do
     solver = Solver.new()
   end
 
-  context "factorial(n) should return the factorial of n" do
+  context "The method factorial(n) should return the factorial of n" do
 
     it "thorws error when n is less than 0" do
       expect{solver.factorial(-1)}.to raise_exception ArgumentError
@@ -23,12 +23,22 @@ describe Solver do
     end
   end
 
-  context "return a reverse for a given string" do
+  context "The method reverse(string) should return a reverse for a given string" do
     it "return the reverse of 'hello' string" do
       result = solver.reverse("hello")
       expected_result = "olleh"
       expect(result).to eql(expected_result)
     end
   end
+
+  context "The method fizzbuzz(n) should return a strung based on some rules" do
+    it "returns 'fizz' when n is divisible by 3" do
+      result = solver.reverse(9)
+      expected_result = "fizz"
+      expect(result).to eql(expected_result)
+    end
+  end
+  
+
 
 end
