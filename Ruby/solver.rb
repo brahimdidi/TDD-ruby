87a92314e34b767152afe81400c355e3b065e202
Ahmad Zamir Yousufi <ahmadzamiryousofi1397@gmail.com>
2022-08-11 22:00:56 +0430

    add a method fizzbuzz to check the number and return a vaild output

--- Ruby/solver.rb
+++ Ruby/solver.rb
@@ -11,4 +11,15 @@ class Solver
   def reverse(string)
     string.reverse
   end
+  def fizzbuzz(n)
+    if n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
+    elsif n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    else
+      n.to_s
+    end
+  end
 end
