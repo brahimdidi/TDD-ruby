f31cb9dd5f239acc8344fea9cbc31e32cd33d306
brahimgb1team@gmail.com <brahimgb1team@gmail.com>
2022-08-11 17:51:22 +0000

    fix linter issues change method param n to be num

--- Ruby/solver.rb
+++ Ruby/solver.rb
@@ -1,11 +1,11 @@
 class Solver
-  def factorial(n)
-    if n.zero?
+  def factorial(num)
+    if num.zero?
       1
-    elsif n.negative?
+    elsif num.negative?
       raise 'ArgumentError'
     else
-      n * factorial(n - 1)
+      num * factorial(num - 1)
     end
   end
 
@@ -13,15 +13,15 @@ class Solver
     string.reverse
   end
 
-  def fizzbuzz(n)
-    if (n % 3).zero? && (n % 5).zero?
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
-    elsif (n % 5).zero?
+    elsif (num % 5).zero?
       'buzz'
-    elsif (n % 3).zero?
+    elsif (num % 3).zero?
       'fizz'
     else
-      n.to_s
+      num.to_s
     end
   end
 end
